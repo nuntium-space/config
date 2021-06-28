05f757a11eda9cb9bc4ae8dbab99880fc5534b46
Alex Sandri <me@alexsandri.com>
2021-06-28 19:06:02 +0200

    Add publisher-icons bucket

--- terraform/local/s3.tf
+++ terraform/local/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "web" {
+resource "aws_s3_bucket" "site" {
   bucket = var.site.domain
   acl    = "public-read"
 
@@ -6,4 +6,9 @@ resource "aws_s3_bucket" "web" {
     index_document = "index.html"
     error_document = "index.html"
   }
+}
+
+resource "aws_s3_bucket" "publisher-icons" {
+  bucket = "publisher-icons"
+  acl    = "public-read"
 }
